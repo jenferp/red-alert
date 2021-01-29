f7d6ed91ca1bcdb2dd5464915b05a3c51b8b7904
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-01-29 14:37:04 -0800

    dv - opened camera window

--- camera.py
+++ camera.py
@@ -1,3 +1,23 @@
 import cv2
-import sys
-import numpy as np
+
+# Opening the camera code from https://subscription.packtpub.com/book/application_development/9781785283932/3/ch03lvl1sec28/accessing-the-webcam
+# Likely modify this? https://docs.opencv.org/master/db/d28/tutorial_cascade_classifier.html
+
+cap = cv2.VideoCapture(0)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    # If the user presses "esc", close the video window.
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
