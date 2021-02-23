e6cb814248262dddab6766b807dc2ba5a84c49a5
jenferp <77474432+jenferp@users.noreply.github.com>
2021-02-22 16:17:11 -0800

    webscraper updated

--- webscraper.py
+++ webscraper.py
@@ -5,48 +5,50 @@ import os
 import urllib.request
 import time
 
-PATH = '/Users/jenniferpark/Desktop/chromedriver'
-save_folder = 'train-images'
+keyword_list = ['tomato', 'meat', 'bloodstain']
+n_images = 2
 
-def main():
-    if not os.path.exists(save_folder):
-        os.mkdir(save_folder)
-    download_images()
+def download_images(keyword_list, n_images):
 
-def download_images():
-
-    keyword = input('what are you looking for? ')
-    n_images = int(input('how many images do you want? '))
-
+    PATH = os.path.relpath('chromedriver')
     driver = webdriver.Chrome(PATH)
     driver.get('https://images.google.com/')
     search = driver.find_element_by_name('q')
-    search.send_keys(keyword)
-    search.send_keys(Keys.RETURN)
 
-    count = 0
-    for i in range(3):
-        driver.execute_script("scrollBy("+ str(count) + ",+1000);")
-        count += 1000
-        time.sleep(1)
+    folder = 'train-images'
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
-    islmp = driver.find_element_by_id('islmp')
-    img = islmp.find_elements_by_tag_name('img')
+    for keyword in keyword_list:
+        search.send_keys(keyword)
+        search.send_keys(Keys.RETURN)
 
-    for j,i in enumerate(img):
-        if j < n_images:
-            src = i.get_attribute('src')
-            try:
-                if src != None:
-                    src = str(src)
-                    print(src)
-                    urllib.request.urlretrieve(src, os.path.join(save_folder, keyword+str(j)+ '.jpg'))
-                else:
-                    raise TypeError
-            except Exception as e:
-                print('fail')
+        # scroll 3 times (to get shit ton of images)
+        count = 0
+        for i in range(3):
+            driver.execute_script("scrollBy(" + str(count) + ",+1000);")
+            count += 1000
+            time.sleep(1)
+
+            # find element
+            islmp = driver.find_element_by_id('islmp')
+            img = islmp.find_elements_by_tag_name('img')
+
+            # get the image
+            for j,i in enumerate(img):
+                if j < n_images:
+                    src = i.get_attribute('src')
+                    try:
+                        if src != None:
+                            src = str(src)
+                            print(src)
+                            subfolder = '{}'.format(keyword)
+                            os.makedirs(subfolder)
+                            urllib.request.urlretrieve(src, os.path.join(subfolder, keyword+str(j)+ '.jpg'))
+                        else:
+                            raise TypeError
+                    except:
+                        print('fail')
     driver.close()
-
-if __name__ == '__main__':
-    main()
-
+    
+    download_images(keyword_list, n_images)
